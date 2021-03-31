befe7a4bada27bb22aa4598bc3a85f49033c6954
karzansherwani <karzan-sherwani@live.no>
2021-03-31 12:06:58 +0200

    setonclicklistener

--- app/src/main/java/com/example/amiajoketoyouu/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/amiajoketoyouu/ui/home/HomeFragment.kt
@@ -1,18 +1,32 @@
 package com.example.amiajoketoyouu.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.amiajoketoyouu.R
+import com.example.amiajoketoyouu.ui.notifications.NotificationsFragment
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var cardviewAny: CardView
+    private lateinit var cardviewDark: CardView
+    private lateinit var cardviewSpooky: CardView
+    private lateinit var cardviewPun: CardView
+    private lateinit var cardviewProgramming: CardView
+    private lateinit var cardviewMisc: CardView
+    var navc : NavController ?= null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +36,46 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-       //
+
+        cardviewAny = root.findViewById(R.id.imagecard_CategoryAny)
+        cardviewDark= root.findViewById(R.id.imageCard_CategoryDark)
+        cardviewMisc = root.findViewById(R.id.imageCard_CategoryMisc)
+        cardviewPun = root.findViewById(R.id.imagecard_CategorPun)
+        cardviewProgramming = root.findViewById(R.id.imageCard_CategoryProgramming)
+        cardviewSpooky = root.findViewById(R.id.imagecard_CategorSpooky)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
           //  textView.text = it
         })
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    SetonClicklistnerCard()
+        }
+    private fun SetonClicklistnerCard () {
+        cardviewAny.setOnClickListener {
+
+        }
+        cardviewDark.setOnClickListener {
+
+            }
+        cardviewSpooky.setOnClickListener {
+
+        }
+        cardviewProgramming.setOnClickListener {
+
+        }
+        cardviewPun.setOnClickListener {
+
+        }
+        cardviewMisc.setOnClickListener {
+
+        }
+    }
+
+    private fun navigateToJokePage() {
+    }
+
+
 }
