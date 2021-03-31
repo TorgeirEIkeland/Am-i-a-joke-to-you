39b8e7f76b8b9310070272d6b0ef99260d824dd6
Alex <moon_haven@hotmail.com>
2021-03-31 11:53:58 +0200

    Added save functionality, did some cleanup

--- app/src/main/java/com/example/amiajoketoyouu/Joke.kt
+++ app/src/main/java/com/example/amiajoketoyouu/Joke.kt
@@ -1,10 +1,5 @@
 package com.example.amiajoketoyouu
 
-import android.content.Context
-import android.util.Log
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 
 val gson = Gson()
@@ -28,23 +23,6 @@ class Joke(
         public fun urlCreator(category: String, searchParameter:String?):String{
             return if(searchParameter != null) "https://v2.jokeapi.dev/joke/${category.toLowerCase().capitalize()}?$searchParameter"
             else "https://v2.jokeapi.dev/joke/${category.toLowerCase().capitalize()}"
-        }
-
-        public fun fetchJoke(url:String, context: Context, callback:(Joke) -> Unit){
-            val newJoke:Joke
-
-            val queue = Volley.newRequestQueue(context)
-
-            val stringRequest = StringRequest(Request.Method.GET, url, {
-                    response ->
-                val newJoke= fromJson(response)
-                callback(newJoke)
-                //Log.d("FOO", newJoke.joke.toString())
-                Log.d("FOO", response)
-
-            }, { Log.d("FOO", "Error occured") })
-
-            queue.add(stringRequest)
         }
     }
 
